c93e2d888b9f0bc9b2283e2eb970a8834bc44774
aslaha@microsoft.com <ashislaha@ASHISs-MacBook-Pro.local>
2023-10-03 22:33:56 -0700

    first commit

---
+++ react_game_demo/src/Message.tsx
@@ -0,0 +1,21 @@
+// tsx is used for typescript + react file
+// normal ts is used for normal typescript purpose
+// we will create component using "function based approach"
+// A component can have a behavior/Property.
+
+// learn how to build component
+// rendering markup with jsx
+// managing state
+// passing input via props
+// debugging react applications
+
+
+// PascalCasing
+function Message() {
+    // JSX: Javascript XML
+    const name = "Ashis";
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
+
